6c9254cdffe14cb554f72a3c7500af4307c8eede
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2024-01-20 11:34:20 +0000

    saturday's day working

--- yandex/room_reservation/app/schemas/meeting_room.py
+++ yandex/room_reservation/app/schemas/meeting_room.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class MeetingRoomBase(BaseModel):
@@ -10,6 +10,17 @@ class MeetingRoomBase(BaseModel):
 
 class MeetingRoomCreate(MeetingRoomBase):
     name: str = Field(..., min_length=1, max_length=100)
+
+
+class MeetingRoomUpdate(MeetingRoomBase):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    
+
+    @validator('name')
+    def name_cant_be_none(cls, value: str):
+        if value is None:
+            raise ValueError('Имя переговорки не может быть пустым!')
+        return value 
 
 
 class MeetingRoomDB(MeetingRoomCreate):
