6d4402357a58f4238646f682081765eb4ac8326d
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2024-02-03 15:10:07 +0000

    there was added a new endpoint that works with google tables

--- yandex/room_reservation/app/api/routers.py
+++ yandex/room_reservation/app/api/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import meeting_room_router, reservation_router, user_router
+from app.api.endpoints import meeting_room_router, reservation_router, user_router, google_api_router
 
 main_router = APIRouter()
 main_router.include_router(
@@ -8,6 +8,10 @@ main_router.include_router(
 )
 main_router.include_router(
     reservation_router, prefix='/reservations', tags=['Reservations']
+)
+# Подключаем импортированный роутер
+main_router.include_router(
+    google_api_router, prefix='/google', tags=['Google']
 )
 main_router.include_router(
     user_router
--- yandex/room_reservation/app/crud/reservation.py
+++ yandex/room_reservation/app/crud/reservation.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pytz
 
-from sqlalchemy import and_, select
+from sqlalchemy import and_, between, func, or_, select 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
@@ -68,6 +68,26 @@ class CRUDReservation(CRUDBase):
         reservations = reservations.scalars().all()
 
         return reservations
+    
+
+    # Новый метод
+    async def get_count_res_at_the_same_time(
+            self,
+            from_reserve: datetime,
+            to_reserve: datetime,
+            session: AsyncSession,
+    ) -> list[dict[str, int]]:
+        reservations = await session.execute(
+            # Получаем количество бронирований переговорок за период
+            select([Reservation.meetingroom_id,
+                    func.count(Reservation.meetingroom_id)]).where(
+                Reservation.from_reserve >= from_reserve,
+                Reservation.to_reserve <= to_reserve
+            ).group_by(Reservation.meetingroom_id)
+        )
+        reservations = reservations.all()
+        return reservations
+
 
 
 reservation_crud = CRUDReservation(Reservation)
